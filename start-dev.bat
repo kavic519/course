b3dc112b8ec0ff7f9daf5abd54de9a346ca636f8
avic k <kavic519@outlook.com>
2025-11-02 16:36:54 +0800

    v1.1.0

--- start-dev.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 启动开发环境 (H2)...
-set SPRING_PROFILES_ACTIVE=dev
-mvn spring-boot:run -Dspring-boot.run.profiles=dev
-pause
--- start-prod.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 启动生产环境 (MySQL)...
-set SPRING_PROFILES_ACTIVE=prod
-mvn spring-boot:run -Dspring-boot.run.profiles=prod
-pause
